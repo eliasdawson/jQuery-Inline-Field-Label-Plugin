da5e1d620eae1d4f3bfb3339b5959ba4b5e5611a
Elias Dawson <elias.echols@gmail.com>
2012-01-10 12:52:07 -0600

    Version 1.0 Modifications

--- jquery-inlinefieldlabel.js
+++ jquery-inlinefieldlabel.js
@@ -1,5 +1,5 @@
 /*
- * jQuery Inline Field Label Plugin
+ * jQuery Inline Field Label Plugin v1.0
  *
  * Copyright 2012 Elias Dawson
  * Licensed under the MIT license
@@ -22,47 +22,55 @@
 		}
 
 		this.each( function() {
-			var label = $( this ),
-				fieldID = label.attr( "for" );
-
-			if ( fieldID !== undefined ) {
-				var tempPasswordField,
-					field = $( "#" + fieldID ),
-					isTextField = field.attr( "type" ) === "text",
-					isPasswordField = field.attr( "type" ) === "password";
-		
+			var tempPasswordField,
+				label = $( this ),
+				field = $( document.getElementById( label.attr( "for" ) ) );
+			
+			//Only run if for attribute is present and a valid ID
+			if ( field.length ) {
+				
+				//Hide <label> element
 				label.hide();
-				if ( isTextField ) {
+				
+				//Text field
+				if ( field.is( "textarea" ) || field.attr( "type" ) === "text" ) {
+					//Set input element value and add CSS class
 					field.val( label.text() ).addClass( config.cssClass );
+					
+					//Bind focus and blur events
+					field.focus(function() {
+						if ( field.val() === label.text() ) {
+							field.val( "" ).removeClass( config.cssClass );
+						}
+					}).blur(function() {
+						if ( field.val() === "" ) {
+							field.val( label.text() ).addClass( config.cssClass );
+						}
+					});
 				}
-				else if ( isPasswordField ) {
-					tempPasswordField = $( "<input>" ).attr( "type", "text" ).addClass( field.attr("class") );
-					tempPasswordField.val(label.text()).addClass(config.cssClass);
+				//Password field
+				else if ( field.attr( "type" ) === "password" ) {
+					//Create input element to simulate plain text password field
+					tempPasswordField = $( "<input>" ).attr( "type", "text" ).addClass( field.attr( "class" ) );
+					tempPasswordField.val( label.text() ).addClass( config.cssClass );
+					
+					//Add element to DOM
 					field.hide();
-					tempPasswordField.insertBefore(field);
+					tempPasswordField.insertBefore( field );
+					
+					//Bind focus and blur events
 					tempPasswordField.focus(function() {
 						tempPasswordField.hide();
 						field.show().focus();
 					});
-				}
-
-				field.focus(function() {
-					if ( isTextField ) {
-						if ( field.val() === label.text() ) {
-							field.val( "" ).removeClass( config.cssClass );
-						}
-					}
-				}).blur(function() {
-					if ( field.val() === "" ) {
-						if ( isTextField ) {
-							field.val( label.text() ).addClass( config.cssClass );
-						}
-						else if ( isPasswordField ) {
+					
+					field.blur(function() {
+						if ( field.val() === "" ) {
 							tempPasswordField.show();
 							field.hide();
 						}
-					}
-				});
+					});
+				}
 			}
 		});
 
